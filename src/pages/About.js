2c4dc93f4ef26cbac413c141085be3d1edf1c17a
keaganeng <eng00029@umn.edu>
2022-06-12 11:32:16 -0500

    Finished week 1 lab

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./About.css";
+import img from "../assets/Keagan_Eng.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src={img}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Keagan Eng</div>
+            <div className="brief_description">
+              <p>School: University of Minnesota - Twin Cities</p>
+              <p>Major: Computer Science</p>
+              <p>Fun Fact: I learned how to downhill ski this year!</p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
